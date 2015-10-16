4389d58a2c4ef9f18f26014b4e8fbf0b9d043b79
kazlauskis <karolis@kazlauskis.com>
2015-10-16 13:54:59 +0100

    Update init scripts

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,69 +2,95 @@
  * App object.
  *****************************************************************************/
 define([
-    'jquery',
-    'jquery.mobile',
-    'backbone',
-    'fastclick',
-    'klass',
-    'routers/router',
-    'models/app',
-    'models/user',
-    'models/species',
-    'models/record',
-    'helpers/update',
-    'helpers/brcart',
-    'helpers/message',
-    'helpers/log',
-    'data'
-  ],
-  function ($, jqm, Backbone, FastClick, klass, Router, AppModel, UserModel,
-            SpeciesCollection, RecordModel, update, brcArt) {
-    var App = {
-      init: function () {
-        //init Google Analytics
-        //http://veithen.github.io/2015/02/14/requirejs-google-analytics.html
-        if (app.CONF.GA.STATUS){
-          window.GoogleAnalyticsObject = "__ga__";
-          window.__ga__ = {
-            q: [["create", app.CONF.GA.ID, "auto"]],
-            l: Date.now()
-          };
-          require(['ga'], function(ga) {
-            ga('set', 'appName', app.NAME);
-            ga('set', 'appVersion', app.VERSION);
-          });
-        }
+        'jquery',
+        'jquery.mobile',
+        'backbone',
+        'fastclick',
+        'klass',
+        'morel',
+        'routers/router',
+        'models/app',
+        'models/user',
+        'models/species',
+        'helpers/update',
+        'helpers/brcart',
+        'helpers/browser',
+        'helpers/message',
+        'helpers/log',
+        'data'
+    ],
+    function ($, jqm, Backbone, FastClick, klass, morel, Router, AppModel, UserModel,
+              SpeciesCollection, update, brcArt, browser, message) {
+        var App = {
+            init: function () {
+                app.browser = browser;
+                app.message = message;
 
-        _log(brcArt, log.INFO);
+                //init Google Analytics
+                //http://veithen.github.io/2015/02/14/requirejs-google-analytics.html
+                if (app.CONF.GA.STATUS){
+                    window.GoogleAnalyticsObject = "__ga__";
+                    window.__ga__ = {
+                        q: [["create", app.CONF.GA.ID, "auto"]],
+                        l: Date.now()
+                    };
+                    require(['ga'], function(ga) {
+                        ga('set', 'appName', app.NAME);
+                        ga('set', 'appVersion', app.VERSION);
+                        ga('set', 'dimension1', morel.VERSION);
+                    });
+                }
 
-        //overwrite morel user append function to match backbone
-        window.morel.auth.getUser = function () {
-          return app.models.user.attributes;
+                _log(brcArt, log.INFO);
+
+                //overwrite morel user append function to match backbone
+                morel.Auth.prototype.getUser = function () {
+                    return app.models.user.attributes;
+                };
+
+                //init data
+                app.recordManager = new morel.Manager(app.CONF.morel);
+
+                app.models = {};
+                app.models.user = new UserModel();
+                app.models.app = new AppModel();
+                app.models.sample = null; //to be set up on record opening
+                app.models.sampleMulti = null; //to be set up on multi-record page init
+                app.collections = {};
+                app.collections.species = new SpeciesCollection(app.data.species);
+
+                //update app
+                update();
+
+                app.router = new Router();
+                Backbone.history.start();
+
+                FastClick.attach(document.body);
+
+                //turn off the loading splash screen
+                $('div.loading').css('display', 'none');
+                $('body').removeClass('loading');
+
+                //add more variables to Google Analytics
+                if (app.CONF.GA.STATUS) {
+                    require(['ga'], function(ga) {
+                        var userFilters = app.models.user.get('filters');
+                        var favourites = userFilters.favouritesGroup && userFilters.favouritesGroup.length,
+                            type = userFilters.typeGroup && userFilters.typeGroup.length,
+                            color = userFilters.colorGroup && userFilters.colorGroup.length;
+
+                        ga('set', {
+                            'dimension2': (app.models.user.hasSignIn() ? 'signed': ''),
+                            'dimension3': app.models.user.get('autosync') ? 'sync': '',
+                            'dimension4': app.models.user.get('sort'),
+
+                            'metric1': favourites,
+                            'metric2': type,
+                            'metric3': color
+                        });
+                    });
+                }
+            }
         };
-
-        //init data
-        app.models = {};
-        app.models.user = new UserModel();
-        app.models.app = new AppModel();
-        app.models.record = new RecordModel();
-        app.collections = {};
-        app.collections.species = new SpeciesCollection(app.data.species);
-
-        //update app
-        update();
-
-        app.router = new Router();
-        Backbone.history.start();
-
-        //app.fixIOSbuttons();
-
-        FastClick.attach(document.body);
-
-        //turn off the loading splash screen
-        $('div.loading').css('display', 'none');
-        $('body').removeClass('loading');
-      }
-    };
-    return App;
-  });
+        return App;
+    });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,44 +2,49 @@
  * RequireJS configuration and app object construction.
  *****************************************************************************/
 (function () {
-  require.config({
-    baseUrl: "scripts/",
-    paths: {
-      'jquery': 'libs/jquery.min',
-      'jquery.mobile': 'libs/jquery.mobile-1.4.5.min',
-      'IndexedDBShim': 'libs/IndexedDBShim.min',
-      'latlon': 'libs/osgridref.min',
-      'latlon-ellipsoidal': 'libs/latlon-ellipsoidal.min',
-      'vector3d': 'libs/vector3d.min',
-      'dms': 'libs/dms.min',
-      'klass': 'libs/klass.min',
-      'photoswipe': 'libs/code.photoswipe.jquery-3.0.5.min',
-      'fastclick': 'libs/fastclick.min',
-      'd3': 'libs/d3.min',
-      'topojson': 'libs/topojson.min',
-      'morel': 'libs/morel',
-      'underscore': 'libs/lodash.min',
-      'backbone': 'libs/backbone.min',
-      'backbone.localStorage': 'libs/backbone.localStorage-min',
-      'tripjs': 'libs/trip.min',
-      'ga': '//www.google-analytics.com/analytics'
-    },
-    shim: {
-      'latlon': {deps: ['latlon-ellipsoidal', 'vector3d', 'dms']},
-      'jquery.mobile': {deps: ['jquery.mobile-config']},
-      'backbone': {deps: ['jquery', 'underscore'], "exports": "Backbone"},
-      'morel': {deps: ['IndexedDBShim']},
-      'photoswipe': {deps: ['jquery', 'klass'], exports : 'Code.PhotoSwipe'},
-      'topojson': {deps: ['d3']},
-      'ga': {exports: "__ga__"}
-    },
-    waitSeconds: 20
-  });
+    require.config({
+        baseUrl: "scripts/",
+        paths: {
+            'conf': '', //replaced with grunt
+            'jquery': 'libs/jquery.min',
+            'jquery.mobile': 'libs/jquery.mobile-1.4.5.min',
+            'IndexedDBShim': 'libs/IndexedDBShim.min',
+            'latlon': 'libs/osgridref.min',
+            'latlon-ellipsoidal': 'libs/latlon-ellipsoidal.min',
+            'vector3d': 'libs/vector3d.min',
+            'dms': 'libs/dms.min',
+            'klass': 'libs/klass.min',
+            'photoswipe': 'libs/code.photoswipe.jquery-3.0.5.min',
+            'fastclick': 'libs/fastclick.min',
+            'd3': 'libs/d3.min',
+            'topojson': 'libs/topojson.min',
+            'morel': 'libs/morel',
+            'underscore': 'libs/lodash.min',
+            'backbone': 'libs/backbone.min',
+            'backbone.localStorage': 'libs/backbone.localStorage-min',
+            'tripjs': 'libs/trip.min',
+            'touchswipe': 'libs/jquery.touchSwipe.min',
+            'plugin':'libs/jquery.plugin.min',
+            'datepick':'libs/jquery.datepick.min',
+            'ga': '//www.google-analytics.com/analytics'
+        },
+        shim: {
+            'latlon': {deps: ['latlon-ellipsoidal', 'vector3d', 'dms']},
+            'jquery.mobile': {deps: ['jquery.mobile-config']},
+            'backbone': {deps: ['jquery', 'underscore'], "exports": "Backbone"},
+            'morel': {deps: ['IndexedDBShim']},
+            'photoswipe': {deps: ['jquery', 'klass'], exports : 'Code.PhotoSwipe'},
+            'topojson': {deps: ['d3']},
+            'datepick': {deps: ['plugin']},
+            'ga': {exports: "__ga__"}
+        },
+        waitSeconds: 20
+    });
 
-  //Load the mighty app :)
-  require(['jquery.mobile-config', 'app'], function (jqm, App) {
-    //jquery mobile - backbone configuration should be set up by this point.
-    App.init();
-  });
+    //Load the mighty app :)
+    require(['jquery.mobile-config', 'app'], function (jqmConf, App) {
+        //jquery mobile - backbone configuration should be set up by this point.
+        App.init();
+    });
 
 })();
