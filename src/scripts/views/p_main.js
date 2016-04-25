7fd29e67b0da26489d2e506e380c52927d6efc59
kazlauskis <karolis@kazlauskis.com>
2016-04-25 13:57:32 +0100

    Remove appcache

--- src/scripts/views/p_main.js
+++ src/scripts/views/p_main.js
@@ -3,12 +3,9 @@
  *****************************************************************************/
 define([
     'views/_page',
-    'views/dialog_download',
-    'views/dialog_add_homescreen',
     'helpers/browser',
     'templates'
-], function (DefaultPage, download, addHomescreenDialog,
-             browser) {
+], function (DefaultPage, browser) {
     'use strict';
 
     var Page = DefaultPage.extend({
@@ -16,17 +13,11 @@ define([
 
         template: app.templates.p_main,
 
-        events: {
-            'click #download-button': 'tripDownload'
-        },
-
         initialize: function () {
             _log('views.MainPage: initialize', log.DEBUG);
 
             this.render();
             this.appendEventListeners();
-
-            this.trip();
         },
 
         render: function () {
@@ -36,50 +27,11 @@ define([
 
             $('body').append($(this.el));
 
-            //disable/enable downloading
-            this.$footer = this.$el.find('#main-footer');
-
-            var downloaded = app.models.user.get('downloaded-app');
-            if (downloaded) {
-                this.$footer.hide();
-
-            //silent desktop download
-            } else if (!browser.isMobile()) {
-                this.$footer.hide();
-                if(app.CONF.OFFLINE.STATUS){
-                    download(null, true);
-                }
-            }
-
             return this;
         },
 
         appendEventListeners: function () {
             this.appendBackButtonListeners();
-        },
-
-        /**
-         * Shows the user around the page.
-         */
-        trip: function () {
-            var callback = function () {
-                app.views.mainPage.$footer.hide();
-            };
-            var downloaded = app.models.user.get('downloaded-app');
-            if (!downloaded && browser.isIOS() && browser.isHomeMode()) {
-                download(callback);
-            }
-        },
-
-        tripDownload: function () {
-            var callback = function () {
-                app.views.mainPage.$footer.hide();
-            };
-            addHomescreenDialog(function () {
-                if (app.CONF.OFFLINE.STATUS) {
-                    download(callback);
-                }
-            });
         }
     });
 
